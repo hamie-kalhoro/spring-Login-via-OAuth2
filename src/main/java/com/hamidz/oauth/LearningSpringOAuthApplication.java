9698d35576e00700cb995365bb13b3766c79ce60
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-06 20:37:00 +0500

    initial files with dependencies from spring.io

---
+++ src/main/java/com/hamidz/oauth/LearningSpringOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hamidz.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamidz/oauth/LearningSpringOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamidz.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
