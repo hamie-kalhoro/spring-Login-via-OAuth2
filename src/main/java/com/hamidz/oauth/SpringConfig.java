872ea83396bfb35eed49645ad93974ba8d305f54
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-06 22:24:12 +0500

    adds oauth2login using java 8 for directly accessing profile after singning via gmail as given in properties

--- src/main/java/com/hamidz/oauth/SpringConfig.java
+++ src/main/java/com/hamidz/oauth/SpringConfig.java
@@ -1,10 +1,17 @@
 package com.hamidz.oauth;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 public class SpringConfig {
@@ -17,7 +24,12 @@ public class SpringConfig {
                     registry.requestMatchers("/").permitAll();
                     registry.anyRequest().authenticated();
                 }).
-                oauth2Login(Customizer.withDefaults()).
+                oauth2Login(oauth2login -> {
+                    oauth2login.successHandler((request,
+                                                response,
+                                                        authentication) ->
+                            response.sendRedirect("/profile"));
+                }).
                 formLogin(Customizer.withDefaults()).
                 build();
     }
